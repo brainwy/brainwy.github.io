488410da1010d70e3744d17bc8f179ea9efbe822
fabioz <fabiofz@gmail.com>
2018-02-28 15:32:31 -0300

    Updated to 4.5.1

--- scripts/build.py
+++ scripts/build.py
@@ -15,20 +15,20 @@ import warnings
 # On a new release, update the links and version.
 
 DOWNLOADS = '''
-http://www.mediafire.com/file/gp1gyvgijlsj89g/liclipse_4.4.0_linux.gtk.x86.tar.gz
-http://www.mediafire.com/file/qy1r5ahxgaamc81/liclipse_4.4.0_linux.gtk.x86_64.tar.gz
-http://www.mediafire.com/file/8rdtsqoik412hjf/liclipse_4.4.0_macosx.cocoa.x86_64.dmg
-http://www.mediafire.com/file/smi4urrgkazziqf/liclipse_4.4.0_win32.x86_64.exe
-http://www.mediafire.com/file/odbay98ocot23se/liclipse_4.4.0_win32.x86.exe
-http://www.mediafire.com/file/0q4936x764d17q0/UPDATE_SITE_4.4.0.zip
-http://www.mediafire.com/file/lvx1oxjocznzy3j/SHA256_AND_INSTALL_INSTRUCTIONS.txt
-http://www.mediafire.com/file/0gz3vv71bbvrobd/LICENSE.TXT
-https://www.mediafire.com/folder/mt1qpbk83pcgd/LiClipse_4.4.0
+http://www.mediafire.com/file/32rf5fht851ncs2/LICENSE.TXT
+http://www.mediafire.com/file/v527h9pca5nf5uy/liclipse_4.5.1_linux.gtk.x86.tar.gz
+http://www.mediafire.com/file/fl2xgbczyb1zj74/liclipse_4.5.1_linux.gtk.x86_64.tar.gz
+http://www.mediafire.com/file/8c4m8xh7v380pul/liclipse_4.5.1_macosx.cocoa.x86_64.dmg
+http://www.mediafire.com/file/u79162wmb399n56/liclipse_4.5.1_win32.x86.exe
+http://www.mediafire.com/file/lou53d6ad6xq2zu/liclipse_4.5.1_win32.x86_64.exe
+http://www.mediafire.com/file/4x0yczwb96s9xd4/SHA256_AND_INSTALL_INSTRUCTIONS.txt
+http://www.mediafire.com/file/if7hoofx5xjca54/UPDATE_SITE_4.5.1.zip
+http://www.mediafire.com/folder/dwad1rmz3sh4f/LiClipse_4.5.1
 '''
 
 DOWNLOAD_REPLACEMENTS = {
     'all_versions_url': 'https://www.mediafire.com/folder/ka5iei6qnyaq4/LiClipse',
-    'liclipse_version': '4.4.0',
+    'liclipse_version': '4.5.1',
 }
 
 help_location = r'X:\liclipse\plugins\com.brainwy.liclipse.help'
